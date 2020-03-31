87251827ab3ab821fc94cecc4b113013004bb865
kasios <princekasios@gmail.com>
2020-03-31 21:00:18 +0200

    Fully reviewed Databank and DataModel documentation.

--- src/main/java/es/upm/etsisi/cf4j/data/DataModel.java
+++ src/main/java/es/upm/etsisi/cf4j/data/DataModel.java
@@ -1,47 +1,48 @@
 package es.upm.etsisi.cf4j.data;
 
 import es.upm.etsisi.cf4j.data.types.DataSetEntry;
-
 import java.io.Serializable;
 import java.util.*;
 
 /**
- * <p>Class that manages all information of the Collaborative Filtering kernel. Contains the users and
- * items sets imported from the database.</p>
- *
- * <p>This class can not be instantiated. It implements the singleton pattern, so, when we want to use
- * it, we must use the getInstance() method.</p>
+ * This class manages all the information related with a collaborative filtering based recommender system.
+ * A DataModel should be instantiated from a DataSet. This class also provides the possibility to manage serialized
+ * files based on instances of this class.
  * @author Fernando Ortega, Jesús Mayor
  */
 public class DataModel implements Serializable {
 
     private static final long serialVersionUID = 20200314L;
 
+    //Stored users or items instances with his related ratings.
     private User[] users;
-    private TestUser[] testUsers;
-
     private Item[] items;
+
+    //Stored users or items instances with his related test ratings.
+    private TestUser[] testUsers;
     private TestItem[] testItems;
 
-    //Stored metrics
+    //Stored metrics.
     private double minRating = Double.MAX_VALUE;
     private double maxRating = Double.MIN_VALUE;
     private int numberOfRatings = 0;
     private double ratingAverage = 0.0;
 
-    //Stored test metrics
+    //Stored test metrics.
     private double minTestRating = Double.MAX_VALUE;
     private double maxTestRating = Double.MIN_VALUE;
     private int numberOfTestRatings = 0;
     private double testRatingAverage = 0.0;
 
-
-
-
+    //Heterogeneous data storage.
     private DataBank dataBank;
 
     /**
-     * This constructor initializes the DataModel with the contents the given DataSet.
+     * This constructor initializes the DataModel with the contents of the given DataSet.
+     * Data stored in the dataset is splitted in 4 different arrays: user, testUser, item and testItems. Keeping the
+     * main behaviour of the Recommendation Systems in mind, a testUser could have voted a regular item or an TestItem.
+     * Similarly, a testItem could have been voted by a regular user or a testUser.
+     * In the other hand, regular users or items wont have a test part.
      * @param dataset DataSet to be added to the DataModel.
      */
     public DataModel (DataSet dataset){
@@ -157,7 +158,8 @@ public class DataModel implements Serializable {
     }
 
     /**
-     * This method adds a single test rating to the DataSet
+     * This method adds a single test rating to the DataModel.
+     * This method is also in charge of recalculate the general test metrics: minTestRating, maxTestRating and ratingTestAverage.
      * @param testUserIndex UserCode as string, of the rating.
      * @param testItemIndex ItemCode to be rated.
      * @param rating Rating of the item.
@@ -177,9 +179,10 @@ public class DataModel implements Serializable {
     }
 
     /**
-     * This method adds a single rating to the DataSet.
-     * @param userIndex UserCode as string, of the rating.
-     * @param itemIndex ItemCode to be rated.
+     * This method adds a single rating to the DataModel.
+     * This method is also in charge of recalculate the general metrics: minRating, maxRating and ratingAverage.
+     * @param userIndex User index as integer, of the rating.
+     * @param itemIndex Item index to be rated.
      * @param rating Rating of the item.
      */
     public void addRating (int userIndex, int itemIndex, double rating) {
@@ -198,6 +201,7 @@ public class DataModel implements Serializable {
 
     /**
      * Getter associated with the array of users
+     * If you need a specific element it is recommended to use the getUser() method.
      * @return users array
      */
     public User[] getUsers() {
@@ -206,6 +210,7 @@ public class DataModel implements Serializable {
 
     /**
      * Getter associated with the array of test users
+     * If you need a specific element it is recommended to use the getTestUser() method.
      * @return test users array
      */
     public TestUser[] getTestUsers() {
@@ -214,6 +219,7 @@ public class DataModel implements Serializable {
 
     /**
      * Getter associated with the array of items
+     * If you need a specific element it is recommended to use the getItem() method.
      * @return items array
      */
     public Item[] getItems() {
@@ -222,6 +228,7 @@ public class DataModel implements Serializable {
 
     /**
      * Getter associated with the array of test items
+     * If you need a specific element it is recommended to use the getTestItem() method.
      * @return test items array
      */
     public TestItem[] getTestItems() {
@@ -229,7 +236,7 @@ public class DataModel implements Serializable {
     }
 
     /**
-     * Getter of the stored data. This data allows you to store general calculation data inside the DataModel.
+     * Getter of the databank. This data allows you to store general calculation data inside the DataModel.
      * @return The databank who stores general information.
      */
     public DataBank getDataBank(){
@@ -237,26 +244,26 @@ public class DataModel implements Serializable {
     }
 
     /**
-     * Get the number of users
-     * @return Number of users
+     * Get the number of users contained in the datamodel.
+     * @return Number of users.
      */
     public int getNumberOfUsers () {
         return this.users.length;
     }
 
     /**
-     * Get an user by his  userIndex
-     * @param userIndex Index of the users array inside the datamodel
-     * @return User located at userIndex
+     * Get an user by his userIndex.
+     * @param userIndex Index of the users' array inside the datamodel.
+     * @return User located at given userIndex.
      */
     public User getUser(int userIndex) {
         return users[userIndex];
     }
 
     /**
-     * Get the  userIndex of an user at the users array
-     * @param userCode User code
-     * @return  Index if the user exists or -1 if doesn't
+     * Find the userIndex of a user at the users' array given an userCode.
+     * @param userCode User code to be searched.
+     * @return  Index if the user exists or -1 if doesn't.
      */
     public int findUserIndex(String userCode) {
         for ( int i = 0; i < this.users.length; i++)
@@ -267,24 +274,24 @@ public class DataModel implements Serializable {
     }
 
     /**
-     * Get the number of test users
-     * @return Number of test users
+     * Get the number of test users contained in the datamodel.
+     * @return Number of test users.
      */
     public int getNumberOfTestUsers() { return this.testUsers.length; }
 
     /**
-     * Get an user by his  userIndex
-     * @param testUserIndex Index of the testUsers array inside the datamodel
-     * @return TestUser located at given userIndex
+     * Get a test user by his testUserIndex.
+     * @param testUserIndex Index of the testUsers' array inside the datamodel.
+     * @return TestUser located at given testUserIndex.
      */
     public TestUser getTestUser(int testUserIndex) {
         return testUsers[testUserIndex];
     }
 
     /**
-     * Get the userIndex of a test user at the test users array
-     * @param testUserCode User code
-     * @return  Index if the user exists or -1 if doesn't
+     * Find the testUserIndex of a test user at the testUsers' array given a testUserCode.
+     * @param testUserCode Test user code to be searched.
+     * @return  Index if the user exists or -1 if doesn't.
      */
     public int findTestUserIndex(String testUserCode) {
         for ( int i = 0; i < this.testUsers.length; i++)
@@ -295,24 +302,24 @@ public class DataModel implements Serializable {
     }
 
     /**
-     * Get the number of items
-     * @return Number of items
+     * Get the number of items contained in the datamodel.
+     * @return Number of items.
      */
     public int getNumberOfItems() { return this.items.length; }
 
     /**
-     * Get an item by his  userIndex
-     * @param itemIndex Index of the items array inside the datamodel
-     * @return Item located at given userIndex
+     * Get an item by his itemIndex.
+     * @param itemIndex Index of the items' array inside the datamodel.
+     * @return Item located at given itemIndex.
      */
     public Item getItem(int itemIndex) {
         return this.items[itemIndex];
     }
 
     /**
-     * Get the  userIndex of a item at the test items array
-     * @param itemCode Item code
-     * @return  Index if the item exists or -1 if doesn't
+     * Find the itemIndex of a item at the items' array given an itemCode.
+     * @param itemCode Item code to be searched.
+     * @return  Index if the item exists or -1 if doesn't.
      */
     public int findItemIndex(String itemCode) {
         for ( int i = 0; i < this.items.length; i++)
@@ -323,24 +330,24 @@ public class DataModel implements Serializable {
     }
 
     /**
-     * Get the number of test items
-     * @return Number of test items
+     * Get the number of test items contained in the datamodel.
+     * @return Number of test items.
      */
     public int getNumberOfTestItems() { return this.testItems.length; }
 
     /**
-     * Get a test item by his  userIndex
-     * @param testItemIndex Code of the test item to retrieve
-     * @return TestItem located at given userIndex
+     * Get a test item by his testItemIndex.
+     * @param testItemIndex Index of the test items' array inside the datamodel.
+     * @return TestItem located at given itemIndex.
      */
     public TestItem getTestItem(int testItemIndex) {
         return this.testItems[testItemIndex];
     }
 
     /**
-     * Get the userIndex of a test item at the test items array
-     * @param testItemCode Test item code
-     * @return Index if the item exists or -1 if doesn't
+     * Find the testItemIndex of a test item at the test items' array given a testItemCode.
+     * @param testItemCode Test item code to be searched.
+     * @return Index if the item exists or -1 if doesn't exist.
      */
     public int findTestItemIndex(String testItemCode) {
         for ( int i = 0; i < this.testItems.length; i++)
@@ -351,64 +358,64 @@ public class DataModel implements Serializable {
     }
 
     /**
-     * Get the minimum rating done
-     * @return minimum rating
+     * Get the minimum rating done (test ratings are not included).
+     * @return minimum rating.
      */
     public double getMinRating(){
         return minRating;
     }
 
     /**
-     * Get the maximum rating done
-     * @return maximum rating
+     * Get the maximum rating done (test ratings are not included).
+     * @return maximum rating.
      */
     public double getMaxRating(){
         return maxRating;
     }
 
     /**
-     * Get the average of ratings
-     * @return average
+     * Get the average of ratings (test ratings are not included).
+     * @return average of ratings.
      */
     public double getRatingAverage(){
         return this.ratingAverage;
     }
 
     /**
-     * Get the minimum rating done
-     * @return minimum rating
+     * Get the minimum test rating done.
+     * @return minimum test rating.
      */
     public double getMinTestRating(){
         return this.minTestRating;
     }
 
     /**
-     * Get the maximum rating done
-     * @return maximum rating
+     * Get the maximum test rating done.
+     * @return maximum test rating.
      */
     public double getMaxTestRating(){
         return this.maxTestRating;
     }
 
     /**
-     * Get the average of test ratings
-     * @return average
+     * Get the average of test ratings.
+     * @return average of test ratings.
      */
     public double getTestRatingAverage(){
         return this.testRatingAverage;
     }
 
     /**
-     * Return the number of ratings
-     * @return number of ratings
+     * Return the number of ratings contained in the datamodel.
+     * @return number of ratings.
      */
     public int getNumberOfRatings() {
         return this.numberOfRatings;
     }
 
     /**
-     * Return the number of test ratings
-     * @return number of test ratings
+     * Return the number of test ratings contained in the datamodel.
+     * @return number of test ratings.
      */
     public int getNumberOfTestRatings() {
         return this.numberOfTestRatings;
