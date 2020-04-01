80aeec59e729ff90fc6c63fd0464ee969deb2818
Fernando Ortega <fortegarequena@gmail.com>
2020-04-01 16:05:27 +0200

    Fixed typos in DataSet documentation

--- src/main/java/es/upm/etsisi/cf4j/data/DataSet.java
+++ src/main/java/es/upm/etsisi/cf4j/data/DataSet.java
@@ -4,11 +4,12 @@ import es.upm.etsisi.cf4j.data.types.DataSetEntry;
 import java.util.Iterator;
 
 /**
- * This interface work as a bridge between the raw file and the DataModel (defined memory structure to finally use the
- * recommendation system). This interface ensures data separated into two groups: no-test and test.
- * The data, obtained from the files, must be returned in DataSetEntries which structure is conformed by user, item and rating.
+ * This interface works as a bridge between the raw file and the DataModel. This interface ensures collaborative
+ * filtering ratings separated into two groups: no-test and test. The ratings must be returned in DataSetEntries
+ * which structure is conformed by user, item and rating.
  */
-public interface DataSet{
+public interface DataSet {
+
     /**
      * This method generates an iterator to navigate through the raw ratings stored in DataSetEntries.
      * @return Iterator of ratings
@@ -16,8 +17,8 @@ public interface DataSet{
     Iterator<DataSetEntry> getRatingsIterator();
 
     /**
-     * This method indicates the size of the Ratings stored data.
-     * @return Number of elements of the stored data.
+     * This method indicates the number of (training) ratings.
+     * @return Number of (training) ratings
      */
     int getNumberOfRatings();
 
@@ -28,8 +29,8 @@ public interface DataSet{
     Iterator<DataSetEntry> getTestRatingsIterator();
 
     /**
-     * This method indicates the size of the Test Ratings stored data
-     * @return Number of elements of the test stored data.
+     * This method indicates the number of test ratings.
+     * @return Number of test ratings
      */
     int getNumberOfTestRatings();
 }
