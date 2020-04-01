cf3dca311cc1dd63e71180a72ad8c295bcfd1b7b
Fernando Ortega <fortegarequena@gmail.com>
2020-04-01 16:23:36 +0200

    Improved RadomSplitDataSet documentation

--- src/main/java/es/upm/etsisi/cf4j/data/RandomSplitDataSet.java
+++ src/main/java/es/upm/etsisi/cf4j/data/RandomSplitDataSet.java
@@ -11,12 +11,15 @@ import java.util.Random;
 import java.util.TreeMap;
 
 /**
- * This class implements the DataSet interface, this implies that it is in charge of reading a file in a certain way to
- * generate a series of iterators over the data read from the file.
- * Specifically, RandomSplitDataSet will read a file with the data and generate an iterator over the stored memory.
+ * <p></p>This class implements the DataSet interface by random splitting the collaborative filtering ratings allocated
+ * in a text file. Each line of the ratings file must have the following format:</p>
+ * <pre>&lt;userId&gt;&lt;separator&gt;&lt;itemId&gt;&lt;separator&gt;&lt;rating&gt;</pre>
+ *<p>Where &lt;separator&gt; is an special character that delimits ratings fields (semicolon by default).</p>
+ *<p>Training and test ratings are selected randomly by the probability of an user and an item to belong to the test
+ * set.</p>
  */
-public class RandomSplitDataSet implements DataSet
-{
+public class RandomSplitDataSet implements DataSet {
+
     protected static final String DEFAULT_SEPARATOR = ";";
 
     /**
@@ -30,9 +33,7 @@ public class RandomSplitDataSet implements DataSet
     protected ArrayList<DataSetEntry> testRatings;
 
     /**
-     * Generates a DataSet form a text file. It is needed to construct the DataModel.
-     * The lines of the file must have the format: userId;itemId;rating.
-     * The dataset is loaded without test items and test users.
+     * Generates a DataSet form a text file. The DataSet is loaded without test items and test users.
      * @param filename File with the ratings.
      */
     public RandomSplitDataSet (String filename) throws IOException {
@@ -40,9 +41,8 @@ public class RandomSplitDataSet implements DataSet
     }
 
     /**
-     * Generates a DataSet form a text file. It is needed to construct the DataModel.
-     * The lines of the file must have the format: userId;itemId;rating.
-     * The dataset is loaded with a specific percentage of test items and test users.
+     * Generates a DataSet form a text file. The DataSet is loaded with a specific percentage of test items and test
+     * users.
      * @param filename File with the ratings.
      * @param testUsersPercent Percentage of users that will be of test.
      * @param testItemsPercent Percentage of items that will be of test.
@@ -53,10 +53,9 @@ public class RandomSplitDataSet implements DataSet
     }
 
     /**
-     * Generates a DataSet form a text file. It is needed to construct the DataModel.
-     * The lines of the file must have the format: userId;itemId;rating.
-     * The dataset is loaded with a specific percentage of test items and test users.
-     * Using an specific seed the system ensure the test items and test users between executions.
+     * Generates a DataSet form a text file. The DataSet is loaded with a specific percentage of test items and test
+     * users. This constructor allows to define an specific random seed to ensure the reproducibility of the
+     * experiments.
      * @param filename File with the ratings.
      * @param testUsersPercent Percentage of users that will be of test.
      * @param testItemsPercent Percentage of items that will be of test.
@@ -68,9 +67,8 @@ public class RandomSplitDataSet implements DataSet
     }
 
     /**
-     * Generates a DataSet form a text file, filling the dataModel attribute.
-     * The lines of the file must have the format: userId SEPARATOR itemId SEPARATOR rating.
-     * The dataset is loaded with a specific percentage of test items and test users.
+     * Generates a DataSet form a text file. The DataSet is loaded with a specific percentage of test items and test
+     * users.
      * @param filename File with the ratings.
      * @param testUsersPercent Percentage of users that will be of test.
      * @param testItemsPercent Percentage of items that will be of test.
@@ -82,9 +80,7 @@ public class RandomSplitDataSet implements DataSet
     }
 
     /**
-     * Generates a DataSet form a text file, filling the dataModel attribute.
-     * The lines of the file must have the format: userId SEPARATOR itemId SEPARATOR rating.
-     * The dataset is loaded without test items and test users.
+     * Generates a DataSet form a text file. The DataSet is loaded without test items and test users.
      * @param filename File with the ratings.
      * @param separator Separator char between file fields.
      * @throws IOException When the file is not accessible by the system with write permissions.
@@ -94,10 +90,9 @@ public class RandomSplitDataSet implements DataSet
     }
 
     /**
-     * Generates a DataSet form a text file, filling the dataModel attribute.
-     * The lines of the file must have the format: userId SEPARATOR itemId SEPARATOR rating.
-     * The dataset is loaded with a specific percentage of test items and test users.
-     * Using an specific seed the system ensure same test items and test users between executions.
+     * Generates a DataSet form a text file. The DataSet is loaded with a specific percentage of test items and test
+     * users. This constructor allows to define an specific random seed to ensure the reproducibility of the
+     * experiments.
      * @param filename File with the ratings.
      * @param testUsersPercent Percentage of users that will be of test.
      * @param testItemsPercent Percentage of items that will be of test.
@@ -155,34 +150,22 @@ public class RandomSplitDataSet implements DataSet
         datasetFile.close();
     }
 
-    /**
-     * Implementation of the interface DataSet.
-     * @return An interator with the entries. (no test entries)
-     */
+    @Override
     public Iterator<DataSetEntry> getRatingsIterator(){
         return ratings.iterator();
     }
 
-    /**
-     * Implementation of the interface DataSet.
-     * @return An interator with the entries. (test entries)
-     */
+    @Override
     public Iterator<DataSetEntry> getTestRatingsIterator(){
         return testRatings.iterator();
     }
 
-    /**
-     * Implementation of the interface DataSet.
-     * @return Number of elements of the Rating array.
-     */
+    @Override
     public int getNumberOfRatings(){
         return ratings.size();
     }
 
-    /**
-     * Implementation of the interface DataSet.
-     * @return Number of elements of the Test Rating array.
-     */
+    @Override
     public int getNumberOfTestRatings(){
         return testRatings.size();
     }
