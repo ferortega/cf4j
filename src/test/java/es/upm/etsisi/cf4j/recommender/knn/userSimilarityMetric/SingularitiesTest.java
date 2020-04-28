53f40aa2bbc3d26ebe1fb89364f662ee33d52622
kasios <princekasios@gmail.com>
2020-04-28 21:11:00 +0200

    Improved readability in tests

--- src/test/java/es/upm/etsisi/cf4j/recommender/knn/userSimilarityMetric/SingularitiesTest.java
+++ src/test/java/es/upm/etsisi/cf4j/recommender/knn/userSimilarityMetric/SingularitiesTest.java
@@ -2,6 +2,7 @@ package es.upm.etsisi.cf4j.recommender.knn.userSimilarityMetric;
 
 import es.upm.etsisi.cf4j.data.DataModel;
 import es.upm.etsisi.cf4j.data.MockDataSet;
+import es.upm.etsisi.cf4j.data.User;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -22,12 +23,17 @@ class SingularitiesTest {
         Singularities sim = new Singularities(new double[]{0.0,1.0,4.0}, new double[]{2.0,3.0});
         sim.setDatamodel(datamodel);
         sim.beforeRun();
-        assertEquals(0.0703125,sim.similarity(datamodel.getUser(0),datamodel.getUser(1)));
-        assertEquals(0.10286458333333333,sim.similarity(datamodel.getUser(0),datamodel.getUser(3)));
-        assertEquals(0.076171875,sim.similarity(datamodel.getUser(1),datamodel.getUser(3)));
-        assertTrue(sim.similarity(datamodel.getUser(0),datamodel.getUser(1))<sim.similarity(datamodel.getUser(0),datamodel.getUser(3)));
-        assertTrue(sim.similarity(datamodel.getUser(0),datamodel.getUser(1))<sim.similarity(datamodel.getUser(1),datamodel.getUser(3)));
-        assertTrue(sim.similarity(datamodel.getUser(1),datamodel.getUser(3))<sim.similarity(datamodel.getUser(0),datamodel.getUser(3)));
+
+        User user0 = datamodel.getUser(0);
+        User user1 = datamodel.getUser(1);
+        User user2 = datamodel.getUser(3);
+
+        assertEquals(0.0703125,sim.similarity(user0,user1));
+        assertEquals(0.10286458333333333,sim.similarity(user0,user2));
+        assertEquals(0.076171875,sim.similarity(user1,user2));
+        assertTrue(sim.similarity(user0,user1)<sim.similarity(user0,user2));
+        assertTrue(sim.similarity(user0,user1)<sim.similarity(user1,user2));
+        assertTrue(sim.similarity(user1,user2)<sim.similarity(user0,user2));
         sim.afterRun();
     }
 }
