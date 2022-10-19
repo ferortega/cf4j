1f55d4f1f9761b47e0299ea2ce0ab6fa12fc89ba
Fernando Ortega <fortegarequena@gmail.com>
2022-10-19 14:43:58 +0200

    Updated Math asserts to put in the correct place expected and returned values

--- src/test/java/es/upm/etsisi/cf4j/util/Math.java
+++ src/test/java/es/upm/etsisi/cf4j/util/Math.java
@@ -7,19 +7,19 @@ public class Math {
 
     @Test
     void range() {
-        Assertions.assertArrayEquals(Maths.range(2), new int[]{0, 1, 2});
-        Assertions.assertArrayEquals(Maths.range(1, 3), new int[]{1, 2, 3});
-        Assertions.assertArrayEquals(Maths.range(1, 5, 2), new int[]{1, 3, 5});
-        Assertions.assertArrayEquals(Maths.range(1, 6, 2), new int[]{1, 3, 5});
-        Assertions.assertArrayEquals(Maths.range(1, 5, 2, false), new int[]{1, 3});
+        Assertions.assertArrayEquals(new int[]{0, 1, 2}, Maths.range(2));
+        Assertions.assertArrayEquals(new int[]{1, 2, 3}, Maths.range(1, 3));
+        Assertions.assertArrayEquals(new int[]{1, 3, 5}, Maths.range(1, 5, 2));
+        Assertions.assertArrayEquals(new int[]{1, 3, 5}, Maths.range(1, 6, 2));
+        Assertions.assertArrayEquals(new int[]{1, 3}, Maths.range(1, 5, 2, false));
     }
 
     @Test
     void linespace() {
-        Assertions.assertArrayEquals(Maths.linespace(2, 3), new double[]{0.0, 1.0, 2.0});
-        Assertions.assertArrayEquals(Maths.linespace(1, 2, 3), new double[]{1.0, 1.5, 2.0});
-        Assertions.assertArrayEquals(Maths.linespace(0, 1, 2, false), new double[]{0.0, 0.5});
-        Assertions.assertArrayEquals(Maths.linespace(0, 1, 3, true), new double[]{0.0, 0.5, 1.0});
+        Assertions.assertArrayEquals(new double[]{0.0, 1.0, 2.0}, Maths.linespace(2, 3));
+        Assertions.assertArrayEquals(new double[]{1.0, 1.5, 2.0}, Maths.linespace(1, 2, 3));
+        Assertions.assertArrayEquals(new double[]{0.0, 0.5}, Maths.linespace(0, 1, 2, false));
+        Assertions.assertArrayEquals(new double[]{0.0, 0.5, 1.0}, Maths.linespace(0, 1, 3, true));
     }
 
     @Test
