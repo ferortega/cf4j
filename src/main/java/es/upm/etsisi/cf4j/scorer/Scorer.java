6f82928d78aa27f1bd576da0f330a0593482fe55
Fernando Ortega <fortegarequena@gmail.com>
2024-03-06 23:50:23 +0100

    Added export methods to Scorer

--- src/main/java/es/upm/etsisi/cf4j/scorer/Scorer.java
+++ src/main/java/es/upm/etsisi/cf4j/scorer/Scorer.java
@@ -6,7 +6,11 @@ import es.upm.etsisi.cf4j.util.Maths;
 import es.upm.etsisi.cf4j.util.process.Parallelizer;
 import es.upm.etsisi.cf4j.util.process.Partible;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.Map;
 
 /**
  * Abstract class used to simplify the evaluation of collaborative filtering based recommendation
@@ -63,6 +67,84 @@ public abstract class Scorer {
     long sampleSize = Arrays.stream(this.usersScores).filter(score -> !Double.isNaN(score)).count();
     double standardError = standardDeviation / Math.sqrt(sampleSize);
     return coef * standardError;
+  }
+
+  /**
+   * Exports results of Scorer in csv format
+   *
+   * @param filename File name
+   * @throws IOException When file is not found or is locked.
+   */
+  public void exportResults(String filename) throws IOException {
+    exportResults(filename, true);
+  }
+
+  /**
+   * Exports results of Scorer in csv format
+   *
+   * @param filename File name
+   * @param includeHeader Include CSV header line. By default: true
+   * @throws IOException When file is not found or is locked.
+   */
+  public void exportResults(String filename, boolean includeHeader) throws IOException {
+    exportResults(filename, ",", includeHeader);
+  }
+
+  /**
+   * Exports results of Scorer in csv format
+   *
+   * @param filename File name
+   * @param separator CSV separator field. By default: colon character (,)
+   * @throws IOException When file is not found or is locked.
+   */
+  public void exportResults(String filename, String separator) throws IOException {
+    exportResults(filename, separator, true);
+  }
+
+  /**
+   * Exports results of Scorer in csv format
+   *
+   * @param filename File name
+   * @param separator CSV separator field. By default: colon character (,)
+   * @param includeHeader Include CSV header line. By default: true
+   * @throws IOException When file is not found or is locked.
+   */
+  public void exportResults(String filename, String separator, boolean includeHeader) throws IOException {
+    File f = new File(filename);
+    File parent = f.getAbsoluteFile().getParentFile();
+    if (!parent.exists() && !parent.mkdirs()) {
+      throw new IOException("Unable to create directory " + parent);
+    }
+
+    PrintWriter writer = new PrintWriter(f);
+
+    if (includeHeader) {
+      writer.print("testUserIndex");
+      writer.print(separator);
+      writer.print("userId");
+      writer.print(separator);
+
+      String scorerName = this.getClass().getSimpleName().toLowerCase();
+      writer.print(scorerName);
+
+      writer.println();
+    }
+
+    for (int testUserIndex = 0; testUserIndex < usersScores.length; testUserIndex++) {
+      writer.print(testUserIndex);
+      writer.print(separator);
+
+      TestUser testUser = recommender.getDataModel().getTestUser(testUserIndex);
+      String userId = testUser.getId();
+      writer.print(userId);
+      writer.print(separator);
+
+      writer.print(usersScores[testUserIndex]);
+
+      writer.println();
+    }
+
+    writer.close();
   }
 
   /** Private inner class used to parallelize the computation of the quality measures */
